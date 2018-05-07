d6cdd26937584ec25699ad8aac296179ef106f45
Killercat <rayershih@gmail.com>
2018-05-07 09:09:41 +0800

    Add list_broadcasts and credential processing

---
+++ backend-server/LiveStreamControllerPOC.py
@@ -0,0 +1,87 @@
+#!/usr/bin/python
+
+import argparse
+import os
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+from oauth2client.file import Storage
+from oauth2client import client
+from oauth2client import tools
+
+CLIENT_SECRETS_FILE = 'client_secret.json'
+
+SCOPES = ['https://www.googleapis.com/auth/youtube.readonly']
+API_SERVICE_NAME = 'youtube'
+API_VERSION = 'v3'
+APPLICATION_NAME = 'U2ber'
+
+VALID_BROADCAST_STATUSES = ('all', 'active', 'completed', 'upcoming',)
+
+
+# Authorize the request and store authorization credentials.
+def get_authenticated_service(flags):
+    credentials = get_credentials(flags)
+    return build(API_SERVICE_NAME, API_VERSION, credentials=credentials)
+
+
+# Retrieve a list of broadcasts with the specified status.
+def list_broadcasts(youtube_client, broadcast_status):
+    print('Broadcasts with status "%s":' % broadcast_status)
+
+    list_broadcasts_request = youtube_client.liveBroadcasts().list(
+        broadcastStatus=broadcast_status,
+        part='id,snippet',
+        maxResults=50
+    )
+
+    while list_broadcasts_request:
+        list_broadcasts_response = list_broadcasts_request.execute()
+
+        print('Response : {}'.format(list_broadcasts_response))
+
+        for broadcast in list_broadcasts_response.get('items', []):
+            print('%s (%s)' % (broadcast['snippet']['title'], broadcast['id']))
+
+        list_broadcasts_request = youtube.liveBroadcasts().list_next(
+            list_broadcasts_request, list_broadcasts_response)
+
+
+# def get_live_chats(youtube_client):
+#     request = youtube_client.liveChatMessages().list(liveChatId='abc123', part='id,snippet')
+#     response = request.execute()
+#     print('{0}'.format(response))
+#
+
+
+def get_credentials(args):
+    home_dir = os.path.expanduser('~')
+    credential_dir = os.path.join(home_dir, '.credentials')
+    if not os.path.exists(credential_dir):
+        os.makedirs(credential_dir)
+    credential_path = os.path.join(credential_dir, 'u2ber.json')
+
+    store = Storage(credential_path)
+    credentials = store.get()
+    if not credentials or credentials.invalid:
+        flow = client.flow_from_clientsecrets(CLIENT_SECRETS_FILE, SCOPES)
+        flow.user_agent = APPLICATION_NAME
+        credentials = tools.run_flow(flow, store, args)
+        print('Storing credentials to ' + credential_path)
+
+    return credentials
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(parents=[tools.argparser])
+    parser.add_argument('--broadcast-status', help='Broadcast status',
+                        choices=VALID_BROADCAST_STATUSES, default=VALID_BROADCAST_STATUSES[0])
+
+    args = parser.parse_args()
+
+    youtube = get_authenticated_service(args)
+    try:
+        list_broadcasts(youtube, args.broadcast_status)
+        # get_live_chats(youtube)
+        # list_broadcasts(youtube, 'all')
+    except HttpError as e:
+        print('An HTTP error %d occurred:\n%s' % (e.resp.status, e.content))
---
+++ backend-server/README.md
@@ -0,0 +1,10 @@
+# Backend Server
+
+
+## Requirements
+
+Please add your own client_secret.json under this directory. To get it, please refer to [here](https://blog.aweimeow.tw/2016/11/23/第一次用-google-api-就上手/).
+
+## Limitations
+
+Currently, we can only get our own YouTube broadcasts associated on our own Google Account. To de-authorize, please remove `~/.credentials/u2ber.json` and run application again, it will re-authenciate again.
